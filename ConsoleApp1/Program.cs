7e83366c4732598c0aea3caa3832aa2d9fc10cea
Helerson Lage <helersonlage@hotmail.com>
2019-10-19 19:07:31 +1300

    Sample Methods Statics, void,

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var people = new Entity();
+
+            people.Age = Entity.StaticGetAge_1(new DateTime(1978,01,01));
+            Console.WriteLine($"Age Static is {people.Age}");
+
+            people.Age = people.NonStaticReturnGetAge(new DateTime(1979, 01, 01));
+            Console.WriteLine($"Age Static is {people.Age}");
+
+            people.NonStaticVoidGetAge_(new DateTime(1980, 01, 01));
+            Console.WriteLine($"Age Static is {people.Age}");
+
+            Console.ReadLine();
         }
+    }
+}
+
+
+
+
+namespace ConsoleApp1
+{
+    public class Entity
+    {        
+        public int Age { get; set; }
+
+        public static int StaticGetAge_1(DateTime birthDay)
+        {
+            return  DateTime.Now.Year - birthDay.Year;
+        }
+
+
+        public int NonStaticReturnGetAge(DateTime birthDay)
+        {
+            return DateTime.Now.Year - birthDay.Year;
+        }
+
+
+        public void NonStaticVoidGetAge_(DateTime birthDay)
+        {
+            Age = DateTime.Now.Year - birthDay.Year;
+        }
+
+
+
     }
 }
